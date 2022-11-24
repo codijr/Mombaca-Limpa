9408b7eeaf8e5858dd6640537c2c3a9b86a8cb13
kingnaldoo <reinaldosn2016@gmail.com>
2022-11-24 00:20:17 -0300

    feature/add ios vector icons

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 import SplashScreen from "react-native-splash-screen";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function App() {
+  Icon.loadFont();
+
   useEffect(() => {
     SplashScreen.hide();
   });
@@ -10,6 +13,7 @@ export default function App() {
   return (
     <View>
       <Text style={{ fontFamily: "LeagueSpartan-Bold" }}>Hello World</Text>
+      <Icon name="alarm" size={20} color="#000000" />
     </View>
   );
 }
