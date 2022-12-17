c9edc8b15f286ef25f535df955b70d57659d361f
kingnaldoo <reinaldosn2016@gmail.com>
2022-12-17 16:01:43 -0300

    fix/layout ajustments

--- src/screens/private/TabProfile/Profile/index.tsx
+++ src/screens/private/TabProfile/Profile/index.tsx
@@ -1,41 +1,17 @@
 import React from "react";
-import { Text } from "react-native";
-import {
-  ContainerProfile,
-  Content,
-  ProfileContent,
-  ProfileButtons,
-  TextProf,
-  ButtonsContent,
-} from "./style";
-import Icon from "react-native-vector-icons/Feather";
-import { theme } from "../../../../global/styles/theme";
+import { ProfileButton } from "./components/ProfileButton";
+import { Container } from "./style";
 
 export function Profile() {
   return (
-    <ContainerProfile>
-      <ProfileContent>
-        <Content>
-          <ButtonsContent>
-            <ProfileButtons>
-              <Icon name="mail" size={35} color={theme.colors.textGray} />
-              <TextProf>Alterar email</TextProf>
-            </ProfileButtons>
-            <ProfileButtons>
-              <Icon name="lock" size={35} color={theme.colors.textGray} />
-              <TextProf>Alterar senha</TextProf>
-            </ProfileButtons>
-            <ProfileButtons>
-              <Icon name="info" size={35} color={theme.colors.textGray} />
-              <TextProf>Sobre</TextProf>
-            </ProfileButtons>
-            <ProfileButtons>
-              <Icon name="log-out" size={35} color={theme.colors.textGray} />
-              <TextProf>Sair</TextProf>
-            </ProfileButtons>
-          </ButtonsContent>
-        </Content>
-      </ProfileContent>
-    </ContainerProfile>
+    <Container>
+      <ProfileButton title="Alterar email" icon="mail" />
+
+      <ProfileButton title="Alterar senha" icon="lock" />
+
+      <ProfileButton title="Sobre" icon="info" />
+
+      <ProfileButton title="Sair" icon="log-out" />
+    </Container>
   );
 }
--- src/screens/private/TabProfile/Profile/style.ts
+++ src/screens/private/TabProfile/Profile/style.ts
@@ -1,44 +1,6 @@
-import { Dimensions, ScrollView, Text, Touchable, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 import styled from "styled-components";
-import { ms } from "react-native-size-matters";
-import { Container } from "../../../../global/styles/theme";
-import { RFValue } from "react-native-responsive-fontsize";
 
-const heightDimensions = Dimensions.get("window").height;
-
-export const ContainerProfile = styled(Container)`
-  background-color: ${({ theme }) => theme.colors.background};
-  height: ${heightDimensions}px;
-  padding: ${ms(0)}px;
-`;
-
-export const ProfileContent = styled(View)`
+export const Container = styled(View)`
   flex: 1;
 `;
-
-export const Content = styled(View)`
-  height: ${heightDimensions}px;
-  padding: ${ms(0)}px;
-  flex-direction: column;
-`;
-
-export const ButtonsContent = styled(View)`
-  flex-direction: column;
-  padding-bottom: ${ms(20)}px;
-
-`;
-
-
-export const ProfileButtons = styled(TouchableOpacity)`
-  padding: ${ms(10)}px ${ms(18)}px;
-  flex-direction: row;
-  align-items: center;
-`;
-
-export const TextProf = styled(Text)`
-  font-size: ${RFValue(19)}px;
-  font-family: ${({ theme }) => theme.fonts.text300};
-  color: ${({ theme }) => theme.colors.textGray};
-  margin-left: ${ms(20)}px;
-`;
-
